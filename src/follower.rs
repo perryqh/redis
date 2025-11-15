ab66daac60c88964c09c303ad1d4f82273ea596e
Perry Hertler <perry@hertler.org>
2025-11-15 13:44:21 -0600

    Update follower.rs

--- src/follower.rs
+++ src/follower.rs
@@ -30,14 +30,66 @@ impl Follower {
             follower_replication.leader_host, follower_replication.leader_port
         );
         let mut stream = TcpStream::connect(&leader_addr).await?;
-        let (reader, writer) = stream.split();
-        self.ping_leader(reader, writer).await
+        let (mut reader, mut writer) = stream.split();
+        self.ping_leader(&mut reader, &mut writer).await?;
+        self.repl_conf_listening(&mut reader, &mut writer).await?;
+        self.repl_conf_capa(&mut reader, &mut writer).await
+    }
+
+    async fn repl_conf_capa<Reader, Writer>(
+        &self,
+        reader: &mut Reader,
+        writer: &mut Writer,
+    ) -> anyhow::Result<()>
+    where
+        Reader: AsyncReadExt + Unpin,
+        Writer: AsyncWriteExt + Unpin,
+    {
+        let conf_array = Array::new(vec![
+            Box::new(BulkString::new("REPLCONF".to_string())),
+            Box::new(BulkString::new("capa".to_string())),
+            Box::new(BulkString::new("psync2".to_string())),
+        ]);
+        writer.write_all(&conf_array.to_bytes()?).await?;
+
+        let mut buf = [0; 512];
+        let byte_count = reader.read(&mut buf).await?;
+        ensure!(byte_count > 0, "No data received from master");
+        let bytes = &buf[..byte_count];
+        ensure!(bytes == b"+OK\r\n", "Unexpected response from master");
+        Ok(())
+    }
+
+    async fn repl_conf_listening<Reader, Writer>(
+        &self,
+        reader: &mut Reader,
+        writer: &mut Writer,
+    ) -> anyhow::Result<()>
+    where
+        Reader: AsyncReadExt + Unpin,
+        Writer: AsyncWriteExt + Unpin,
+    {
+        let conf_array = Array::new(vec![
+            Box::new(BulkString::new("REPLCONF".to_string())),
+            Box::new(BulkString::new("listening-port".to_string())),
+            Box::new(BulkString::new(
+                self.app_context.config.server_port.to_string(),
+            )),
+        ]);
+        writer.write_all(&conf_array.to_bytes()?).await?;
+
+        let mut buf = [0; 512];
+        let byte_count = reader.read(&mut buf).await?;
+        ensure!(byte_count > 0, "No data received from master");
+        let bytes = &buf[..byte_count];
+        ensure!(bytes == b"+OK\r\n", "Unexpected response from master");
+        Ok(())
     }
 
     async fn ping_leader<Reader, Writer>(
         &self,
-        mut reader: Reader,
-        mut writer: Writer,
+        reader: &mut Reader,
+        writer: &mut Writer,
     ) -> anyhow::Result<()>
     where
         Reader: AsyncReadExt + Unpin,
@@ -64,9 +116,9 @@ mod tests {
     async fn test_ping_leader() {
         let app_context = AppContext::default();
         let follower = Follower::new(app_context);
-        let result = follower
-            .ping_leader(tokio::io::empty(), tokio::io::sink())
-            .await;
+        let mut reader = tokio::io::empty();
+        let mut writer = tokio::io::sink();
+        let result = follower.ping_leader(&mut reader, &mut writer).await;
         assert!(result.is_err());
     }
 
@@ -74,9 +126,9 @@ mod tests {
     async fn test_ping_leader_success() {
         let app_context = AppContext::default();
         let follower = Follower::new(app_context);
-        let result = follower
-            .ping_leader(std::io::Cursor::new(b"+PONG\r\n"), tokio::io::sink())
-            .await;
+        let mut reader = std::io::Cursor::new(b"+PONG\r\n");
+        let mut writer = tokio::io::sink();
+        let result = follower.ping_leader(&mut reader, &mut writer).await;
         assert!(result.is_ok());
     }
 
@@ -84,9 +136,9 @@ mod tests {
     async fn test_ping_leader_failure() {
         let app_context = AppContext::default();
         let follower = Follower::new(app_context);
-        let result = follower
-            .ping_leader(std::io::Cursor::new(b"-ERR\r\n"), tokio::io::sink())
-            .await;
+        let mut reader = std::io::Cursor::new(b"-ERR\r\n");
+        let mut writer = tokio::io::sink();
+        let result = follower.ping_leader(&mut reader, &mut writer).await;
         assert!(result.is_err());
     }
 }
